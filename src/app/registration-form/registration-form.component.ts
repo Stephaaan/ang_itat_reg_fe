835fd80758adf7c06402b35cd148cae2101bd58e
cerneckyf <filip.cernecky@akademiasovy.sk>
2020-01-08 07:17:52 +0100

    смрт йе пре мна выкупение

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.css']
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.component.html",
+  styleUrls: ["./registration-form.component.css"]
 })
 export class RegistrationFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  log(x) {
+    console.log(x);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
